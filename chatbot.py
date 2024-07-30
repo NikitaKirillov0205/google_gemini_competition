6f45c1be8930c0b3dccc02e6667ce9bd3e282d81
NikitaKirillov0205 <nikitakirillov0205@gmail.com>
2024-07-30 19:49:22 +0200

    Main bot fix

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+genai.configure(api_key='-')
+model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
 hide_img_fs = '''
 <style>
@@ -8,7 +14,7 @@ button[title="View fullscreen"]{
 '''
 st.link_button("Homepage", "https://google.com")
 st.markdown(hide_img_fs, unsafe_allow_html=True)
-st.image('viralistlogo.png', caption=None, width=None, use_column_width=None, clamp=False, channels="RGB",
+st.image('virallogo.png', caption=None, width=None, use_column_width=None, clamp=False, channels="RGB",
          output_format="auto")
 
 if "messages" not in st.session_state:
@@ -19,15 +25,20 @@ for message in st.session_state.messages:
         st.markdown(message['content'])
 
 prompt = st.chat_input("How can I assist you today?")
+st.markdown("Model is turned off right now, contact developer for more details, contacts are on the homepage.")
 
-if prompt:
-    with st.chat_message("user", avatar="bear.png"):
-        st.markdown(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt})
+# Original prompt and parts of code are stored securely, contact developer for more details
 
-    response = f'Echo: {prompt}'
-
-    with st.chat_message("assistant", avatar='rabbit.png'):
-        st.markdown(response)
-
-    st.session_state.messages.append({"role": "assistant", "content": response})
+# if prompt:
+#   with st.chat_message("user", avatar="bear.png"):
+#      st.markdown(prompt)
+# st.session_state.messages.append({"role": "user", "content": prompt, "avatar": "bear.png"})
+#
+#   prompt = "Respond as a qualified social media instructor. User will give you a prompt in a specific format. Format: social network / name of the post / ideas (user can specify or you can generate yourself). User should give information about first 2 cells, if not ask him to do so. Do not respond in this format yourself. You should generate ideas for the third part yourself, based on what user gave you. Your job is to generate description for a post, so it would get viral. Include important hashtags. Don't add additional information. Din't add headings. Don't forget to add necessary spacing and new lines. Here is users request: " + prompt
+#  response = model.generate_content(prompt)
+# response = response.text
+#
+#   with st.chat_message("assistant", avatar='rabbit.png'):
+#      st.markdown(response)
+#
+#   st.session_state.messages.append({"role": "assistant", "content": response, "avatar": "rabbit.png"})
